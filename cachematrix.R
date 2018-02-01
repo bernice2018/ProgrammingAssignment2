52add65e086124ac14f71f0ced27d2c73b752765
admin <admin@admins-MBP.fios-router.home>
2018-02-01 15:23:22 -0500

    Functions makeCachematrix and makeSolve
    #Creation on the functions makeCachematrix and makeSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Creation on the functions makeCachematrix and makeSolve
 
 ## Write a short comment describing this function
+##The  function makeCacheMatrix creates a special "vector", which is really a list 
+#containing a function to:
+# 1- set the value of the matrix
+#  2- get the value of the matrix
+#	3- set the value of the inverse
+#	4-get the value of the inverse
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mat <- NULL
+  set <- function (y){
+    x <<- y
+    mat<<-NULL 
+  }
+  get <- function() x
+  setsolve<- function(solve) mat<<- solve
+  getsolve <- function () mat
+  list( set =set, get=get,
+        setsolve= setsolve,
+        getsolve=getsolve)
+  
 }
 
 
-## Write a short comment describing this function
+# The cacheSolve function computes the inverse of the special "matrix" returned by
+#makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not
+#changed), then the cachesolve should retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  mat <- x$getsolve()
+  if(!is.null(mat)) {
+    message("getting cached data")
+    return(mat)
+  }
+  datamat <- x$get()
+  mat <- solve(datamat, ...)
+  x$setsolve(mat)
+  mat
+  
 }
